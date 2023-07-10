159c45a8cb2cccdf29cf74e6b0c68a11cf0f01dc
DhaneshDDK <dandedhanesh899@gmail.com>
2023-07-10 15:19:59 +0530

    Commit5

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ app.use(express.json());
 
 const cors = require('cors');
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://frontend-one-eta-41.vercel.app/',
     credentials : true
   }));
 
