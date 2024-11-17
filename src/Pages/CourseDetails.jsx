d17011f0a69f2b175cbf718efc8ffc92b4611cb4
DhaneshDDK <121671087+DhaneshDDK@users.noreply.github.com>
2024-11-18 00:15:55 +0530

    Update CourseDetails.jsx

--- src/Pages/CourseDetails.jsx
+++ src/Pages/CourseDetails.jsx
@@ -251,14 +251,14 @@ const CourseDetails = () => {
               <div className="flex items-center gap-4 py-4">
                 <img
                   src={
-                    instructor.image
-                      ? instructor.image
-                      : `https://api.dicebear.com/5.x/initials/svg?seed=${instructor.firstName} ${instructor.lastName}`
+                    instructor?.image
+                      ? instructor?.image
+                      : `https://api.dicebear.com/5.x/initials/svg?seed=${instructor?.firstName} ${instructor?.lastName}`
                   }
                   alt="Author"
                   className="h-14 w-14 rounded-full object-cover"
                 />
-                <p className="text-lg">{`${instructor.firstName} ${instructor.lastName}`}</p>
+                <p className="text-lg">{`${instructor?.firstName} ${instructor?.lastName}`}</p>
               </div>
               <p className="text-richblack-50">
                 {instructor?.additionalDetails?.about}
